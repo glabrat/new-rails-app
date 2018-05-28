e6e913063d63e409759500c94ac19c0c0271119c
gigashock3u <gigashock3u@hotmail.com>
2018-05-27 20:56:29 -0400

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_214658) do
+ActiveRecord::Schema.define(version: 2018_05_27_202231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
 end
